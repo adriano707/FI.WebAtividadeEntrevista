023046c0d41b31dbddb7dffa683268f33adf9df7
Adriano Santana <mano1die2mili@gmail.com>
2023-02-26 09:11:55 -0300

    Add project files.

---
+++ FI.AtividadeEntrevista/DAL/Beneficiarios/DaoBeneficiario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.DAL
+{
+    internal class DaoBeneficiario : AcessoDados
+    {
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="benediciaios">Objeto de cliente</param>
+        internal long Incluir(DML.Beneficiario benediciaios)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Nome", benediciaios.Nome));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Cpf", benediciaios.CPF));
+
+            DataSet ds = base.Consultar("FI_SP_IncClienteV2", parametros);
+            long ret = 0;
+            if (ds.Tables[0].Rows.Count > 0)
+                long.TryParse(ds.Tables[0].Rows[0][0].ToString(), out ret);
+            return ret;
+        }
+
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        internal void Alterar(DML.Beneficiario beneficiario)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Nome", beneficiario.Nome));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("CPF", beneficiario.CPF));
+
+            base.Executar("FI_SP_AltBeneficiario", parametros);
+        }
+    }
+}
---
+++ FI.AtividadeEntrevista/DML/Beneficiario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.DML
+{
+    public class Beneficiario
+    {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        [Required]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// CPF
+        /// </summary>
+        [Required]
+        public string CPF { get; set; }
+
+        /// <summary>
+        /// Id do Cliente
+        /// </summary>
+        [Required]
+        public long IdCliente { get; set; }
+    }
+}
